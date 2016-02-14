590263169c58a49ba94b558f9b8400edae3c7d83
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-02-13 21:47:06 -0800

    parse some json and FB and twitter and linkedin. crushing it.

--- application.js
+++ application.js
@@ -1,6 +1,11 @@
+var stephanie=[]
+var xander = []
 var fullClient = retinaSDK.FullClient("9d562520-d285-11e5-8378-4dad29be0fab")
 $(document).ready(function(){
   getLink()
+  getStephanieFacebook()
+  getXanderFacebook()
+  getXanderLinkedIn()
   compare()
   // getTokens()
 })
@@ -9,30 +14,68 @@ function appendImg(img){
 }
 
 function getLink(){
-  tweets=[]
   $.get("scraped/amuttican.json", function(response){
-    response["results"]["collection1"].map( tweet => tweets.push(tweet["twitter"]))
-  fullClient.getImage({expression: {"text": tweets.join()}}, appendImg)
+    response["results"]["collection1"].map( tweet => stephanie.push(tweet["twitter"]))
+  fullClient.getImage({expression: {"text": stephanie.join()}}, appendImg)
 })}
 
 function compare(){
-  tweets2 = []
+
   $.get("scraped/XPSON.json", function(response){
-    response["results"]["collection1"].map( tweet => tweets2.push(tweet["twitter"]))
-    fullClient.compareImage({expressions: [{"text": tweets.join(" ")}, {"text": tweets2.join(" ")}]}, appendImg)
+    response["results"]["collection1"].map( tweet => xander.push(tweet["twitter"]))
+    fullClient.compareImage({expressions: [{"text": stephanie.join(" ")}, {"text": xander.join(" ")}]}, appendImg)
   })
 }
 
-// function getTokens(){
-//   fullClient.getSimilarTermsForExpression({expression: {"text": tweets[0]}}, log)
-// }
+function getStephanieFacebook(){
+  $.get("smhfb.json", function(response){
+    addFBData(response, stephanie)
+  })
+}
+function getXanderFacebook(){
+  $.ajax({url: "xpFB.json"})
+  .done(function(response){addFBData(response, xander)})
+}
+function getXanderLinkedIn(){
+  $.ajax({url: "xpLN.json"})
+  .done(function(response){
+    addLNData(response, xander)
+  })
+}
+function getStephanieLinkedIn(){
+  $.ajax({url: "xpLN.json"})
+  .done(function(response){
+    addLNData(response, stephanie)
+  })
+}
 function log(response){
   console.log(response)
+
 }
-// function appendContext(context){
-//   console.log(context)
-//   var first = context[0].map( y => y.context_label )
-//   var second = context[1].map( y =>  y.context_label )
-//   console.log(first)
-//   console.log(second)
-// }
+function addLNData(lndata, person) {
+  person.push(lndata.summary)
+  console.log(lndata)
+  lndata.threePastPositions.values.map(job => person.push(job.summary))
+  lndata.volunteer.volunteerExperiences.values.map(job => person.push(job.role))
+  try {lndata.threeCurrentPositions.values.map(job => person.push(job.summary))}
+  catch (e){}
+  console.log(person)
+}
+
+function addFBData(fbdata, person) {
+  // console.log(fbdata)
+  var person = person
+  fbdata.likes.data.map( like => person.push(like.name))
+  person.push(fbdata.about)
+  fbdata.feed.data.map( post => post.message ? person.push(post.message) : person )
+  fbdata.books.data.map( book => person.push(book.name))
+
+  fbdata.music.data.map( artist => person.push( artist.name ))
+  // console.log(fbdata)
+  // console.log(person)
+}
+
+function saveToJson(response){
+  JSON.strigify(response)
+}
+
