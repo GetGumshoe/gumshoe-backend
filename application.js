328e2a8a9049ce820ccb1057dc93a6e5588d5ab6
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-02-14 00:19:42 -0800

    get LN data and set up timers to collect image data

--- application.js
+++ application.js
@@ -1,65 +1,94 @@
 var stephanie=[]
 var xander = []
+var maps = {}
 var fullClient = retinaSDK.FullClient("9d562520-d285-11e5-8378-4dad29be0fab")
 $(document).ready(function(){
-  getLink()
-  getStephanieFacebook()
-  getXanderFacebook()
-  getXanderLinkedIn()
-  compare()
-  // getTokens()
+  getInfo()
+  window.setTimeout(compare, 3000)
 })
+
+var reallyGetInfo = getInfo()
+
+function getInfo(){
+
+    getStephanieFacebook()
+    getXanderFacebook()
+    getXanderLinkedIn()
+    getStephanieLinkedIn()
+    getSMHTwitter()
+    getXTwitter()
+    console.log(stephanie)
+  }
+
 function appendImg(img){
+  // console.log(img)
     $("body").append( "<img id='compare' src='data:image/jpeg;base64,"+ img +"'/>")
 }
 
-function getLink(){
+function getSMHTwitter(){
   $.get("scraped/amuttican.json", function(response){
     response["results"]["collection1"].map( tweet => stephanie.push(tweet["twitter"]))
-  fullClient.getImage({expression: {"text": stephanie.join()}}, appendImg)
-})}
+    return true
+  })
+}
+
+function getXTwitter(){
+  $.get("scraped/XPSON.json", function(response){
+    response["results"]["collection1"].map( tweet => xander.push(tweet["twitter"]) )
+    return true
+  })
+}
 
 function compare(){
-
-  $.get("scraped/XPSON.json", function(response){
-    response["results"]["collection1"].map( tweet => xander.push(tweet["twitter"]))
-    fullClient.compareImage({expressions: [{"text": stephanie.join(" ")}, {"text": xander.join(" ")}]}, appendImg)
-  })
+  getXPmap()
+  getSMHmap()
+  // $.get("scraped/XPSON.json", function(response){
+    // response["results"]["collection1"].map( tweet => xander.push(tweet["twitter"]))
+    fullClient.compareImage({expressions: [{"text": stephanie.join(" ")}, {"text": xander.join(" ")}]}, function(img){
+      maps.comparison = img
+    })
+  // })
 }
 
 function getStephanieFacebook(){
   $.get("smhfb.json", function(response){
     addFBData(response, stephanie)
+    return true
   })
+
 }
 function getXanderFacebook(){
   $.ajax({url: "xpFB.json"})
   .done(function(response){addFBData(response, xander)})
+    return true
 }
 function getXanderLinkedIn(){
   $.ajax({url: "xpLN.json"})
   .done(function(response){
     addLNData(response, xander)
+    return true
   })
 }
 function getStephanieLinkedIn(){
-  $.ajax({url: "xpLN.json"})
+  $.ajax({url: "smhLN.json"})
   .done(function(response){
     addLNData(response, stephanie)
+    return true
   })
 }
+
 function log(response){
   console.log(response)
 
 }
 function addLNData(lndata, person) {
   person.push(lndata.summary)
-  console.log(lndata)
+  // console.log(lndata.threeCurrentPositions.values)
   lndata.threePastPositions.values.map(job => person.push(job.summary))
   lndata.volunteer.volunteerExperiences.values.map(job => person.push(job.role))
   try {lndata.threeCurrentPositions.values.map(job => person.push(job.summary))}
   catch (e){}
-  console.log(person)
+  // console.log(person)
 }
 
 function addFBData(fbdata, person) {
@@ -71,11 +100,37 @@ function addFBData(fbdata, person) {
   fbdata.books.data.map( book => person.push(book.name))
 
   fbdata.music.data.map( artist => person.push( artist.name ))
-  // console.log(fbdata)
-  // console.log(person)
+
 }
 
-function saveToJson(response){
-  JSON.strigify(response)
+function getXPmap(){
+  fullClient.getImage({expression: {"text": xander.join()}}, addToMapsX)
 }
+
+function getSMHmap(){
+  fullClient.getImage({expression: {"text": stephanie.join()}}, addToMapsS)
+}
+
+function addToMapsX(img){
+    maps.xander = img
+}
+function addToMapsS(img){
+    maps.stephanie = img
+}
+
+function compareBulk(){
+  c1=[]
+  c2=[]
+  // xander.map(text => c1.push({"text": text}))
+  // stephanie.map(text => c2.push({"text": text}))
+  fullClient.getKeywordsForText({"text": xander.join(" ")}, function(response){
+    response.map( term => c1.push({"term": term}))
+  })
+  fullClient.getKeywordsForText({"text": stephanie.join(" ")}, function(response){
+    response.map( term => c2.push({"term": term}))
+  })
+  fullClient.compareBulk({comparisons: [c1, c2]})
+}
+
+
 
